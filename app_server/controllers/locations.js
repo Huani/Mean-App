6a9a131c94f5e2e41a2fddf30e3035c17f79c9a6
Huanchang Tan <huanchang.tan.t6351@student.hogent.be>
2016-12-13 01:38:49 +0100

    Angular toegevoegd + bug fix voor addReview

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -14,10 +14,10 @@ var _formatDistance = function (distance) {
   var numDistance, unit;
   if (distance && _isNumeric(distance)) {
     if (distance > 1) {
-      numDistance = parseFloat(distance).toFixed(1);
+      numDistance = parseFloat(distance/1000).toFixed(1);
       unit = 'km';
     } else {
-      numDistance = parseInt(distance * 1000,10);
+      numDistance = parseInt(distance,10);
       unit = 'm';
     }
     return numDistance + unit;
@@ -46,54 +46,19 @@ var _showError = function (req, res, status) {
 };
 
 var renderHomepage = function(req, res, responseBody){
-  var message;
-  if (!(responseBody instanceof Array)) {
-    message = "API lookup error";
-    responseBody = [];
-  } else {
-    if (!responseBody.length) {
-      message = "No places found nearby";
-    }
-  }
   res.render('locations-list', {
     title: 'Loc8r - find a place to work with wifi',
     pageHeader: {
       title: 'Loc8r',
       strapline: 'Find places to work with wifi near you!'
     },
-    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-    locations: responseBody,
-    message: message
-  });
+    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
+   });
 };
 
 /* GET 'home' page */
 module.exports.homelist = function(req, res){
-  var requestOptions, path;
-  path = '/api/locations';
-  requestOptions = {
-    url : apiOptions.server + path,
-    method : "GET",
-    json : {},
-    qs : {
-      lng : -0.7992599,
-      lat : 51.378091,
-      maxDistance : 2000000000000
-    }
-  };
-  request(
-    requestOptions,
-    function(err, response, body) {
-      var i, data;
-      data = body;
-      if (response.statusCode === 200 && data.length) {
-        for (i=0; i<data.length; i++) {
-          data[i].distance = _formatDistance(data[i].distance);
-        }
-      }
-      renderHomepage(req, res, data);
-    }
-  );
+  renderHomepage(req, res);
 };
 
 var getLocationInfo = function (req, res, callback) {
@@ -144,7 +109,8 @@ var renderReviewForm = function (req, res, locDetail) {
   res.render('location-review-form', {
     title: 'Review ' + locDetail.name + ' on Loc8r',
     pageHeader: { title: 'Review ' + locDetail.name },
-    error: req.query.err
+    error: req.query.err,
+    url: req.originalUrl
   });
 };
 
